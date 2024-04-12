85f6283e05131855392293ade1773842bdcd7676
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-04-12 22:07:57 +0300

    POJO Unit test

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+public class JavaHelloWorldTest {
+
+    public void testGetHello() {
+
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+        assert ("Hello World".equals(javaHelloWorld.getHello()));
+    }
+}
