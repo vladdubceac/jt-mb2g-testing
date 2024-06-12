1e199ab2bf95f970bc00f973770e1927e8c8b59b
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-06-12 23:24:24 +0300

    12-06-2024

--- src/test/groovy/guru/springframework/JavaHelloWorldSpecTest.groovy
+++ src/test/groovy/guru/springframework/JavaHelloWorldSpecTest.groovy
@@ -2,10 +2,6 @@ package guru.springframework
 
 import spock.lang.Specification
 
-
-/**
- * Created by jt on 2018-12-12.
- */
 class JavaHelloWorldSpecTest extends Specification {
 
     def "GetHello"() {
