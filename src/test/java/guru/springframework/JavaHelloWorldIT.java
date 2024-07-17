68cc6197c24eaa05dab3730ee02db55df4a716fa
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-07-17 18:06:33 +0300

    Integration tests with Maven Failsafe

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldIT {
+
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT Ran");
+    }
+}
